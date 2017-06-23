1a57854f48b134d4f9036af5b249818d947075b5
samkk2 <samzchicago@gmail.com>
2017-06-23 05:55:44 +0400

    this is my first project

---
+++ javamaven/src/main/java/com/mycomp/mydepot/javamaven/javamaven.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycomp.mydepot.javamaven;
+
+/**
+ *
+ * @author Winuser
+ */
+public class javamaven {
+private String name;
+private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "javamaven{" + "name=" + name + ", age=" + age + '}';
+    }
+
+
+}
